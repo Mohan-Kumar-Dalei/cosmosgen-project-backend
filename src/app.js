25d9a20f92388c2459bd8e0cf4423bff6da5421f
Mohan Kumar Dalei <mohankumardalei2001@gmail.com>
2026-08-21 21:40:06 +0530

    cosmosgen-backend update 2

--- src/app.js
+++ src/app.js
@@ -5,31 +5,36 @@ const technicianRoutes = require('./routes/technician.routes');
 const mapRoutes = require('./routes/map.routes');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-//middlewares
+
+// middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"], 
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://cosmosgen-backend.onrender.com"], 
     methods: ["GET", "POST","PUT", "DELETE","PATCH","UPDATE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
 
+// 1. Health check Route (Base URL ke liye)
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Server is working fine"
+    });
+});
 
-//routes
-app.use('*',(req,res)=>{
-    res.send(200).json({
-        "message":"server is working fine"
-    })
-})
+// 2. Main API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/technician', technicianRoutes);
 app.use('/api/map', mapRoutes);
 
-
-
-
-
+// 3. Catch-all Route
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Route not found"
+    });
+});
 
 module.exports = app;
